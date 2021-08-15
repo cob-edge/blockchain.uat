12fe443b7cd40275433d3de70934d9367cf993ee
Jarvi1 <jarvis.cole1@gmail.com>
2021-08-15 18:25:18 +1000

    Ganache Optimisation Updates

--- contracts/IoT.sol
+++ contracts/IoT.sol
@@ -35,7 +35,7 @@ contract IoT {
   );
 
   constructor() public {
-    createTask(1, "Timestamp: Today Baby Jarvis", "Test desc", "Test entity type", 1, 2, 3, 1, 2);
+    createTask(1, "timestamp", "desc", "entityType", 1, 1, 1, 1, 1);
   }
 
   function createTask(uint _id, string memory _timestamp, string memory _desc, string memory _entityType, uint _v1, uint _v2, uint _v3, uint _latitude, uint _longitude) public {
@@ -43,6 +43,12 @@ contract IoT {
     tasks[taskCount] = Task(_id, _timestamp, _desc, _entityType, _v1, _v2, _v3, _latitude, _longitude);
     emit TaskCreated(_id, _timestamp, _desc, _entityType, _v1, _v2, _v3, _latitude, _longitude);
   }
+
+ 
+
+  
+
+
 
   function toggleCompleted(uint _id) public {
     //Task memory _task = tasks[_id];
--- test/IoT.test.js
+++ test/IoT.test.js
@@ -18,34 +18,47 @@ contract('IoT', (accounts) => {
     const taskCount = await this.ioT.taskCount()
     const task = await this.ioT.tasks(taskCount)
     assert.equal(task.id.toNumber(), taskCount.toNumber())
-    assert.equal(task.timestamp, 'Timestamp: Today Baby Jarvis')
-    assert.equal(task.desc, 'Test desc')
-    assert.equal(task.entityType, 'Test entity type')
+    assert.equal(task.timestamp, 'timestamp')
+    assert.equal(task.desc, 'desc')
+      assert.equal(task.entityType, 'entityType')
     assert.equal(task.v1.toNumber(), 1)
-    assert.equal(task.v2.toNumber(), 2)
-    assert.equal(task.v3.toNumber(), 3)
+    assert.equal(task.v2.toNumber(), 1)
+    assert.equal(task.v3.toNumber(), 1)
     assert.equal(task.latitude.toNumber(), 1)
-    assert.equal(task.longitude.toNumber(), 2)
+    assert.equal(task.longitude.toNumber(), 1)
   })
 
-  
+    
     it('creates tasks', async () => {
-    const result = await this.ioT.createTask(2, 'Timestamp: Today Baby Jarvis', 'Test desc', 'Test entity type', 1, 2, 3, 1, 2)
+                for (var i = 2; i <= 50; i++) {
+                    const result = await this.ioT.createTask(i, 'Timestamp: Today Baby Jarvis', 'Test desc', 'Test entity type', 1, 2, 3, 1, 2)
 
-    const taskCount = await this.ioT.taskCount()
+                    /*
+                    const taskCount = await this.ioT.taskCount()
+            
+                    assert.equal(taskCount, 2)
+                    const event = result.logs[0].args
+            
+                    assert.equal(event.id.toNumber(), 2)
+                    assert.equal(event.timestamp, 'Timestamp: Today Baby Jarvis')
+                    assert.equal(event.desc, 'Test desc')
+                    assert.equal(event.entityType, 'Test entity type')
+                    assert.equal(event.v1.toNumber(), 1)
+                    assert.equal(event.v2.toNumber(), 2)
+                    assert.equal(event.v3.toNumber(), 3)
+                    assert.equal(event.latitude.toNumber(), 1)
+                    assert.equal(event.longitude.toNumber(), 2)
+                    */
 
-    assert.equal(taskCount, 2)
-    const event = result.logs[0].args
+                    sleep(300) //introduced to stop ganache crashing 
+                }
+    })
 
-    assert.equal(event.id.toNumber(), 2)
-    assert.equal(event.timestamp, 'Timestamp: Today Baby Jarvis')
-    assert.equal(event.desc, 'Test desc')
-    assert.equal(event.entityType, 'Test entity type')
-    assert.equal(event.v1.toNumber(), 1)
-    assert.equal(event.v2.toNumber(), 2)
-    assert.equal(event.v3.toNumber(), 3)
-    assert.equal(event.latitude.toNumber(), 1)
-    assert.equal(event.longitude.toNumber(), 2)
-  }) 
-
+    function sleep(milliseconds) {
+        const date = Date.now();
+        let currentDate = null;
+        do {
+            currentDate = Date.now();
+        } while (currentDate - date < milliseconds);
+    }
 })
